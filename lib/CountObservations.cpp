75e9740a6f4a1e504864aa8a34691f5f85cd03bd
David Devecsery <ddevec@umich.edu>
2016-10-25 03:49:08 -0400

    Added file I forgot

---
+++ lib/CountObservations.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 David Devecsery
+ */
+
+#include <map>
+#include <vector>
+
+#include "include/lib/DynAlias.h"
+#include "include/lib/IndirFcnTarget.h"
+#include "include/lib/UnusedFunctions.h"
+#include "include/lib/CallContextPass.h"
+
+#include "llvm/Pass.h"
+#include "llvm/PassSupport.h"
+#include "llvm/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Constants.h"
+#include "llvm/IntrinsicInst.h"
+#include "llvm/Module.h"
+#include "llvm/Support/CFG.h"
+#include "llvm/Support/GetElementPtrTypeIterator.h"
+#include "llvm/Support/InstIterator.h"
+
+static llvm::cl::opt<bool>
+  no_calls("invariant-no-call", llvm::cl::init(false),
+      llvm::cl::value_desc("bool"),
+      llvm::cl::desc("Ignores callstack invariants"));
+
+class CountObservations : public llvm::ModulePass {
+ public:
+  static char ID;
+  CountObservations() : llvm::ModulePass(ID) { }
+
+  void getAnalysisUsage(llvm::AnalysisUsage &usage) const {
+    usage.setPreservesAll();
+    usage.addRequired<CallContextLoader>();
+    usage.addRequired<IndirFunctionInfo>();
+    usage.addRequired<UnusedFunctions>();
+  }
+
+  bool runOnModule(llvm::Module &m) override;
+};
+
+char CountObservations::ID = 0;
+static llvm::RegisterPass<CountObservations>
+  X("count-invariants", "CountObservations", false, false);
+
+bool CountObservations::runOnModule(llvm::Module &) {
+  auto &call_info = getAnalysis<CallContextLoader>();
+  auto &indir_info = getAnalysis<IndirFunctionInfo>();
+  auto &used_info = getAnalysis<UnusedFunctions>();
+
+  llvm::dbgs() << "Unused Info Size: " << used_info.numInvariants() << "\n";
+  llvm::dbgs() << "Indir Info Size: " << indir_info.numInvariants() << "\n";
+  if (!no_calls) {
+    llvm::dbgs() << "Call Info Size: " << call_info.numInvariants() << "\n";
+  }
+
+  // We never modify code
+  return false;
+}
